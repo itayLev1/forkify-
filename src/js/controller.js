9bcc5fef475cc6954023dfdc7cb923087bf3288b
itayLev1 <itaylev.sound@gmail.com>
2024-02-14 19:25:58 +0200

    added wlelome log

--- src/js/controller.js
+++ src/js/controller.js
@@ -324,5 +324,6 @@ const init = function() {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  console.log('Welcome!');
 }
 init();
